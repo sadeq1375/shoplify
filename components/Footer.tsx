34e07a32170f5dff8ac7909c0b20a9f219371fe9
sadeq1375 <123751187+sadeq1375@users.noreply.github.com>
2024-07-27 14:11:27 +0430

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mb-24">
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
       {/* Top */}
-      <div className="flex justify-between gap-24">
+      <div className="flex flex-col md:flex-row justify-between gap-24">
         {/* LEFT */}
-        <div className="w-1/4 flex flex-col gap-8">
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
           <Link href="/">
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
@@ -25,9 +25,66 @@ const Footer = () => {
           </div>
         </div>
         {/* CENTER */}
-        <div className="w-1/2"></div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Woman</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Legal & Privacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
         {/* RIGHT */}
-        <div className="w-1/4"></div>
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>
+            Be the first to get the latest news about trends,promotions and much
+            more
+          </p>
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="Email adress"
+              className="p-4 w-3/4 focus:outline-none"
+            />
+            <button className="w-1/4 bg-lama text-white">Join</button>
+          </div>
+          <span className="font-semibold">Sequre Payment</span>
+          <div className="flex justify-between gap-1">
+            <Image src="/discover.png" width={40} height={40} alt="" />
+            <Image src="/skrill.png" width={40} height={40} alt="" />
+            <Image src="/paypal.png" width={40} height={40} alt="" />
+            <Image src="/mastercard.png" width={40} height={40} alt="" />
+            <Image src="/visa.png" width={40} height={40} alt="" />
+          </div>
+        </div>
       </div>
       {/* BOTTOM */}
       <div className=""></div>
